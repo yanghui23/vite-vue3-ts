6f7f6eaaaaffdf6b94b59118c6e7e1d35335bc9c
fuck-code <lbj23_yanghui@163.com>
2022-07-23 14:37:52 +0800

    wip: test

--- src/store/user.ts
+++ src/store/user.ts
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia';
 
 export const useUserStore = defineStore({
   id: 'user',
+
   state: () => {
     return {
       name: '李四',
     };
   },
+
   actions: {
     updateName(name: any) {
       this.name = name;
